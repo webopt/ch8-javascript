dc3a0bcb80583f80a29a15147e8d4bb03b778779
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2016-04-20 14:30:41 -0500

    Set up starting point.

--- js/behaviors.js
+++ js/behaviors.js
@@ -1,32 +1,95 @@
 document.addEventListener("readystatechange", function(){
-	document.querySelector("html").classList.remove("page-loading");
+	var pageFade = document.querySelector(".pageFade"),
+		modal = document.querySelector(".modal"),
+		statusModal = document.querySelector(".statusModal"),
+		okayButton = document.querySelector("#okayButton"),
+		headerStatus = document.querySelector("#headerStatus"),
+		status = document.querySelector("#status"),
+		closeModalButton = document.querySelector(".closeModal"),
+		schedule = document.querySelector("#schedule"),
+		submitAppointment = document.querySelector(".submitAppointment a");
+
+	document.documentElement.classList.remove("page-loading");
+
+	function animate(selector, duration, property, from, to, units){
+		var element = document.querySelector(selector),
+			endTime = +new Date() + duration,
+			interval = (1000 / 60),
+			progress = function(){
+				var progress = Math.abs(((endTime - +new Date()) / duration) - 1);
+				return (progress * (to - from)) + from;
+			},
+			draw = function(){
+				if(endTime > +new Date()){
+					element.style[property] = progress() + units;
+					setTimeout(draw, interval);
+				}
+				else{
+					element.style[property] = to + units;
+					return;
+				}
+			};
+
+		setTimeout(draw, interval);
+	}
 
 	function openModal(){
 		window.scroll(0, 0);
-		document.querySelector(".pageFade").classList.remove("hide");
-		document.querySelector(".modal").classList.add("open");
+		pageFade.classList.remove("hide");
+		modal.classList.add("open");
+		document.body.classList.add("locked");
+
+		if(isMobile()){
+			animate(".modal", 500, "top", -150, 0, "%");
+		}
+		else{
+			animate(".modal", 500, "top", -150, 10, "%");
+		}
 	}
 
 	function closeModal(){
-		document.querySelector(".modal").classList.remove("open");
-		document.querySelector(".pageFade").classList.add("hide");
-		document.querySelector(".statusModal").classList.remove("show");
-		document.querySelector("body").classList.remove("locked");
+		modal.classList.remove("open");
+		pageFade.classList.add("hide");
+		statusModal.classList.remove("show");
+		document.body.classList.remove("locked");
+
+		if(isMobile()){
+			animate(".modal", 500, "top", 0, -150, "%");
+		}
+		else{
+			animate(".modal", 500, "top", 10, -150, "%");
+		}
+	}
+
+	function isMobile(){
+		if(window.innerWidth <= 500){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	function repositionModal(){
+		if(modal.classList.contains("open")){
+			if(isMobile()){
+				modal.style.top = "0";
+			}
+			else{
+				modal.style.top = "10%";
+			}
+		}
 	}
 
 	// Scheduling modal open
-	document.querySelector("#schedule").addEventListener("click", function(){
-		document.querySelector("body").classList.add("locked");
-		openModal();
-	});
+	schedule.addEventListener("click", openModal);
 
 	// Modal close via click
-	document.querySelector(".closeModal, .pageFade").addEventListener("click", function(){
-		closeModal();
-	});
+	pageFade.addEventListener("click", closeModal);
+	closeModalButton.addEventListener("click", closeModal);
 
 	// Appointment submit behavior
-	document.querySelector(".submitAppointment a").addEventListener("click", function(e){
+	submitAppointment.addEventListener("click", function(e){
 		var formData = new FormData(document.getElementById("appointmentForm"));
 
 		fetch("/js/response.json", {
@@ -35,29 +98,32 @@ document.addEventListener("readystatechange", function(){
 		}).then(function(response){
 			return response.json();
 		}).then(function(data){
-			document.querySelector("#status").innerHTML = data.message;
-			document.querySelector(".statusModal").classList.add("show");
-			document.querySelector(".modal").classList.remove("open");
+			status.innerHTML = data.message;
+			statusModal.classList.add("show");
+			modal.classList.remove("open");
 
 			if(data.status === true){
-				document.querySelector("#okayButton").setAttribute("data-status", "success");
-				document.querySelector("#headerStatus").innerHTML = "Thank You!";
+				okayButton.setAttribute("data-status", "success");
+				headerStatus.innerHTML = "Thank You!";
 			}
 			else{
-				document.querySelector("#okayButton").setAttribute("data-status", "failure");
-				document.querySelector("#headerStatus").innerHTML = "Error";
+				okayButton.setAttribute("data-status", "failure");
+				headerStatus.innerHTML = "Error";
 			}
 		});
 	});
 
-	document.querySelector("#okayButton").addEventListener("click", function(e){
+	okayButton.addEventListener("click", function(e){
 		if(e.target.getAttribute("data-status") === "failure"){
-			document.querySelector(".statusModal").classList.remove("show");
-			document.querySelector(".modal").classList.add("open");
+			statusModal.classList.remove("show");
+			modal.classList.add("open");
 		}
 		else{
-			document.querySelector(".pageFade").classList.add("hide");
-			document.querySelector(".statusModal").classList.remove("show");
+			pageFade.classList.add("hide");
+			statusModal.classList.remove("show");
 		}
 	});
+
+	window.addEventListener("resize", repositionModal);
+	window.onorientationchange = repositionModal;
 });
